9348b024fccfa6d2a1001e0b8d2c55fc23abd2e6
jihadable <umarjihad141004@gmail.com>
2025-10-31 11:05:30 +0700

    create api docs

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const express = require("express")
 const cors = require("cors")
+const path = require("path")
 const DB = require("./database/db")
 const apiRouter = require("./route/route")
 require("dotenv").config()
@@ -10,6 +11,11 @@ app.use(express.json(), cors())
 
 const db = DB()
 app.use("/api", apiRouter(db))
+
+app.get("/openapi.json", (_, res) => {
+    res.sendFile(path.join(__dirname, "../openapi.json"))
+})
+app.use("/docs", express.static(path.join(__dirname, "view", "docs")))
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
